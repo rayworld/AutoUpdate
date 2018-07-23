83a995a01473f7a288d390111e6a977e47e44864
Ray <casp@sina.com>
2018-07-24 07:29:13 +0800

    1.0.0.1

--- AutoUpdate/Program.cs
+++ AutoUpdate/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Windows.Forms;
+using System.Xml;
+using Ray.Framework.AutoUpdate;
 
 namespace AutoUpdate
 {
@@ -15,6 +16,31 @@ namespace AutoUpdate
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            AutoUpdater au = new AutoUpdater();
+            try
+            {
+                au.Update();
+            }
+            catch (WebException exp)
+            {
+                MessageBox.Show(String.Format("无法找到指定资源\n\n{0}", exp.Message), "自动升级", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (XmlException exp)
+            {
+                MessageBox.Show(String.Format("下载的升级文件有错误\n\n{0}", exp.Message), "自动升级", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (NotSupportedException exp)
+            {
+                MessageBox.Show(String.Format("升级地址配置错误\n\n{0}", exp.Message), "自动升级", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (ArgumentException exp)
+            {
+                MessageBox.Show(String.Format("下载的升级文件有错误\n\n{0}", exp.Message), "自动升级", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(String.Format("升级过程中发生错误\n\n{0}", exp.Message), "自动升级", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
             Application.Run(new Form1());
         }
     }
